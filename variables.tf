a9e69ec2e778a3fb690aba33473c562d5e394a04
Marcel <66844942+Bakri-10@users.noreply.github.com>
2024-11-01 14:28:38 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -73,8 +73,11 @@ variable "private_ip_address" {
 }
 
 variable "purpose" {
-  description = "Purpose of the resources, used in naming conventions."
   type        = string
-}
-
-
+  default     = "default"
+  description = "(Required) The VM Role and Sequence for naming use or, submit the desired resource name (one that contains a dash)."
+  validation {
+    condition     = strcontains(var.purpose, "-") ? length(var.purpose) <= 30 : length(var.purpose) <= 10
+    error_message = "(Required) Purpose [VM Role.Sequence] segment cannot exceed 10 characters. Name cannot exceed 80."
+  }
+ }
