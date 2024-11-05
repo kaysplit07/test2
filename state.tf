574d887c1b238bcb87d908400355a206c59e36bc
Marcel <66844942+Bakri-10@users.noreply.github.com>
2024-11-05 17:29:54 +0100

    Create state.tf

---
+++ state.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = var.resource_group_name
+    storage_account_name  = var.storage_account_name
+    container_name        = "terraform-state"
+    key                   = "${var.resource_type}/${var.resource_name}.tfstate"
+  }
+}
+
+
+variable "resource_group_name" {
+  description = "Resource group name for Terraform backend"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Storage account name for Terraform backend"
+  type        = string
+}
+
+variable "resource_type" {
+  description = "Type of resource for organizing state files"
+  type        = string
+}
+
+variable "resource_name" {
+  description = "Dynamically generated resource name based on naming standards"
+  type        = string
+  default     = join("-", ["ari", local.naming.environment, local.env_location.locations_abbreviation, local.purpose_rg, "lbi", local.sequence])
+}
